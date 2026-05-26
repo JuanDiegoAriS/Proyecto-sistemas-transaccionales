4b34816db22997e0cd2bb778269f700b5f60d033
Juan Diego Arias <Juandiegoarias1014@gmail.com>
2026-05-26 00:08:30 -0500

    Correcion conexion BD

--- app/database/conexion.py
+++ app/database/conexion.py
@@ -8,9 +8,11 @@ def conectar():
 
         host="localhost",
 
+        port=3306,
+
         user="root",
 
-        password="tu_contraseña",
+        password="1234",
 
         database="zimbra",
 
